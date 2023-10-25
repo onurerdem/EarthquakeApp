245e02969208f2d48c752158b35140b03de6235a
Onur Erdem <erdem.onur@outlook.com.tr>
2023-10-26 01:54:51 +0300

    SplashScreen added.

---
+++ app/src/main/java/com/onurerdem/earthquakeapp/presentation/splash/SplashViewModel.kt
@@ -0,0 +1,42 @@
+package com.onurerdem.earthquakeapp.presentation.splash
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.onurerdem.earthquakeapp.domain.use_case.get_splash.GetSplashUseCase
+import com.onurerdem.earthquakeapp.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import javax.inject.Inject
+
+@HiltViewModel
+class SplashViewModel @Inject constructor(
+    private val getSplashUseCase: GetSplashUseCase
+) : ViewModel() {
+
+    private val _state = mutableStateOf<SplashState>(SplashState())
+    val state : State<SplashState> = _state
+
+    init {
+        getSplash()
+    }
+
+    private fun getSplash() {
+        getSplashUseCase.executeGetSplash().onEach {
+            when (it) {
+                is Resource.Success -> {
+                }
+
+                is Resource.Error -> {
+                    _state.value = SplashState(error = it.message ?: "Error!")
+
+                }
+
+                is Resource.Loading -> {
+                }
+            }
+        }.launchIn(viewModelScope)
+    }
+}
